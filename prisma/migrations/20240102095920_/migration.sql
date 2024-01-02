2093c40543b7e715f28ad8d4c1deb579e6a141d5
ben132333 <ben.dumoulin13@gmail.com>
2024-01-02 20:10:10 +0100

    cleanup

--- prisma/migrations/20240102095920_/migration.sql
+++ prisma/migrations/20240102095920_/migration.sql
@@ -2,10 +2,11 @@
   Warnings:
 
   - The primary key for the `Search` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `id` on the `Search` table. All the data in the column will be lost.
+  - Changed the type of `time` on the `Search` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
 */
 -- AlterTable
 ALTER TABLE "Search" DROP CONSTRAINT "Search_pkey",
-DROP COLUMN "id",
+DROP COLUMN "time",
+ADD COLUMN     "time" INTEGER NOT NULL,
 ADD CONSTRAINT "Search_pkey" PRIMARY KEY ("userId", "city", "time");
